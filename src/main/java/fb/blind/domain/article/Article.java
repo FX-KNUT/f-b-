9b2fe49be9849cd4286e5169c2c4ae7197eec84d
shinyeongwoon <duddns119@naver.com>
2023-01-05 17:33:58 +0900

    shin git test for error from shinywoon

--- src/main/java/fb/blind/domain/article/Article.java
+++ src/main/java/fb/blind/domain/article/Article.java
@@ -12,7 +12,7 @@ public class Article {
      * title: 제목
      * body : 내용
      * date : 작성일
-     * fileName : file upload 시 file 경로
+     * fileName : file upload 시 file 경로 차후 구현 예정... 일단 경로만 가지고 있기로
      * likes : 좋아요 수  server binding
      * views : 조회 수 server binding
      * userId  : 작성자 id 보관 server binding
